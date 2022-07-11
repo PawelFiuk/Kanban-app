c545ea9679292d8e61669e7af06bc8508c36bc82
Pawel <deflorennno@gmail.com>
2022-07-11 15:40:19 +0200

    Stworzenie szablonow HTML, wyrenderowanie taskow

--- base/views.py
+++ base/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 
-def task_list(request):
-    return HttpResponse('Task list')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
